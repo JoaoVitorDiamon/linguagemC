b7a05a76c4f40db40a14f7f4f62646610cb5b0d0
Joao Vitor <joao.santos2112@etec.sp.gov.br>
2023-09-20 17:49:23 -0300

    LINGUAGEM C

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("Santos e o Maior time do Mundo \n");
+	printf("Prova Pratica e muito melhor\n");
+	printf("Java e pra veio\n");
+		printf("Hello\n");
+	return 0;	
+}
+
